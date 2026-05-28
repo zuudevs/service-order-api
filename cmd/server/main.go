7dc543ecf1caffeae3e19d5669068717cab06552
Rafi Indra Pramudhito Zuhayr <zuudevs@gmail.com>
2026-05-28 11:56:44 +0700

    feat: update backup file path to use environment variable for database location

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -32,7 +32,7 @@ import (
 func backup(
 	driveService *gdrive.DriveService,
 ) {
-	filePath := "storage/database.db"
+	filePath := os.Getenv("DATABASE_LOCAL_PATH")+"database.db"
 
 	fileId := os.Getenv(
 		"GOOGLE_DRIVE_DB_FILE_ID",
