1f56f4f0d449df714463e8af5b40aaf154cae40d
Rafi Indra Pramudhito Zuhayr <zuudevs@gmail.com>
2026-05-29 11:21:41 +0700

    fix: drive init failed: reading auth code EOF

--- scripts/autorun.ps1
+++
@@ -1,8 +0,0 @@
-foreach ($line in Get-Content "$PSScriptRoot/../.env") {
-	if ($line -match '^\s*#' -or $line -match '^\s*$') {
-		continue
-	}
-
-	$key, $value = $line -split '=', 2
-	[System.Environment]::SetEnvironmentVariable($key, $value)
-}
